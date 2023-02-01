f78d5040740404e75dc36e2fad871517ce199d8e
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2023-02-01 19:19:27 +0100

    Initial commit

---
+++ src/main/java/at/technikum/springrestbackend/SpringRestBackendApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.bwengspringrestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwengSpringRestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BwengSpringRestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/springrestbackend/SpringRestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.bwengspringrestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwengSpringRestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
