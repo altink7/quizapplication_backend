7ca83099ce75b4e752b1aaa8f34fbc3f5a386d17
Sebastian Gruenewald <wi21b080@technikum-wien.at>
2023-10-15 18:43:31 +0200

    fixed validation

--- src/main/java/at/technikum/springrestbackend/exceptions/ExceptionHandlerAdvice.java
+++ src/main/java/at/technikum/springrestbackend/exceptions/ExceptionHandlerAdvice.java
@@ -3,8 +3,13 @@ package at.technikum.springrestbackend.exceptions;
 import org.modelmapper.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class ExceptionHandlerAdvice {
@@ -12,7 +17,6 @@ public class ExceptionHandlerAdvice {
     //USER Exceptions
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity handleException(ValidationException e) {
-        // log exception
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(e.getMessage());
@@ -21,9 +25,17 @@ public class ExceptionHandlerAdvice {
     //QUIZ Exceptions
     @ExceptionHandler(QuizException.class)
     public ResponseEntity handleQuizException(QuizException e) {
-        // log exception
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND) //TODO: maybe müssen wir das feiner aufsplitten, weil nicht überall passt NOT_FOUND
                 .body(e.getMessage());
+    }
+
+    //Validation Exceptions
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleValidationException(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        return ResponseEntity.badRequest().body("Field: " + fieldErrors.iterator().next().getField() + "; ErrorMessage: " + fieldErrors.iterator().next().getDefaultMessage());
+
     }
 }
