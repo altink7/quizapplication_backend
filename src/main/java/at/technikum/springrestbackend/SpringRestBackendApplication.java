3a61ea2d46dfe3ccb0228b9d543cb786c2f83f2a
Julian Hoffmann <wi21b050@technikum.at>
2023-10-16 13:43:21 +0200

    added .war packaging

--- src/main/java/at/technikum/springrestbackend/SpringRestBackendApplication.java
+++ src/main/java/at/technikum/springrestbackend/SpringRestBackendApplication.java
@@ -2,11 +2,12 @@ package at.technikum.springrestbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class SpringRestBackendApplication {
+public class SpringRestBackendApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRestBackendApplication.class, args);
