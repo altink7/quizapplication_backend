97271c547ddbc4042b4756d1c4acc5058adb8fc5
Julian Hoffmann <wi21b050@technikum.at>
2023-10-16 12:36:32 +0200

    updated QuizDTO + error messages

--- src/main/java/at/technikum/springrestbackend/dto/QuizDTO.java
+++ src/main/java/at/technikum/springrestbackend/dto/QuizDTO.java
@@ -3,6 +3,7 @@ package at.technikum.springrestbackend.dto;
 import at.technikum.springrestbackend.model.Category;
 import at.technikum.springrestbackend.model.Question;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
@@ -10,7 +11,13 @@ import java.util.List;
 @Data
 @JsonIgnoreProperties({"userStatistic"})
 public class QuizDTO {
+
+    @NotNull(message = "Creator ID cannot be null!")
     private Long creatorId;
+
+    @NotNull(message = "Category cannot be null!")
     private Category category;
+
+    @NotNull(message = "A quiz needs questions!")
     private List<Question> questions;
 }
