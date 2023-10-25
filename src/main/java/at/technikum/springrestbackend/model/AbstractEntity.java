ecbbca936ff89916d752b098d25f77b7902de44a
Sebastian Gruenewald <wi21b080@technikum-wien.at>
2023-10-25 16:07:31 +0200

    deleted unnecessary code

--- src/main/java/at/technikum/springrestbackend/model/AbstractEntity.java
+++ src/main/java/at/technikum/springrestbackend/model/AbstractEntity.java
@@ -7,12 +7,10 @@ import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.proxy.HibernateProxy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -36,20 +34,4 @@ public abstract class AbstractEntity implements Serializable {
     @Column(name = "updated_at")
     @JsonIgnore
     private LocalDateTime updatedAt;
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        AbstractEntity that = (AbstractEntity) o;
-        return getId() != null && Objects.equals(getId(), that.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
